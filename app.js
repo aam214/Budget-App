a3abe5f53cd5526a8cdd775f3b1e9c0564a9ba42
Ariana <152944814+aam214@users.noreply.github.com>
2024-05-24 12:04:22 -0500

    Added ids to budget-total, expense-total, summary-total. Added constructor function in JS file. Targeted each element by querySelector or getElementbyID.

---
+++ app.js
@@ -0,0 +1,18 @@
+class UI {
+  constructor() {
+    this.budgetFeedback = document.querySelector(".budget-feedback");
+    this.expenseFeedback = document.querySelector(".expense-feedback");
+    this.budgetForm = document.getElementById("budget-form");
+    this.budgetInput = document.getElementById("budget-input");
+    this.budgetAmount = document.getElementById("budget-total");
+    this.expenseAmount = document.getElementById("expense-total");
+    this.balance = document.getElementById("balance-summary");
+    this.balanceAmount = document.getElementById("balance-total");
+    this.expenseForm = document.getElementById("expense-form");
+    this.expenseInput = document.getElementById("expense-input");
+    this.amountInput = document.getElementById("amount-input");
+    this.expenseList = document.getElementById("expense-list");
+    this.itemList = [];
+    this.itemID = 0;
+  }
+}
