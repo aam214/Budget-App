017c9d9b6906b7040feee239724d5b9ee35d5c40
Ariana <152944814+aam214@users.noreply.github.com>
2024-07-26 12:49:46 -0500

    Changed name in JS from amountInput to memoInput. Renamed array for listItem. Changed method name to enterExpenses. Readded Font Awesome script at the head. Removed button in expense-section. Replaced h4 element in form and made it a label element. Added Bootstrap element to expense-message and budget-message.

--- app.js
+++ app.js
@@ -10,36 +10,36 @@ class UI {
     this.balanceTotal = document.getElementById("balance-total");
     this.expenseForm = document.getElementById("expense-form");
     this.expenseInput = document.getElementById("expense-input");
-    this.amountInput = document.getElementById("amount-input");
+    this.memoInput = document.getElementById("memo-input");
     this.expenseList = document.getElementById("expense-list");
-    this.itemList = [];
+    this.listItem = [];
     this.itemID = 0;
   }
 //submit budget method
  enterBudget() {
   const value= this.budgetInput.value;
-  if (value=== "" || value < 0) {
+  if (value === "" || value < 0) {
     this.budgetMessage.classList.add("showMessage");
     this.budgetMessage.innerHTML = 
     `<div>Please enter a value. You cannot enter a negative number.</div>`;
     const refer = this;
-    //console.log(this);
-
-    setTimeout(function() {
+    setTimeout(() => {
       refer.budgetMessage.classList.remove("showMessage");
-    }, 5000)
+    }, 5000);
 } else{
   this.budgetTotal.textContent = value;
-  this.budgetInput.value = '';
+  this.budgetInput.value = "";
   this.showBalance();
+
 }
 }
-showBalance(){
- const expense = this.sumofExpenses();
+
+
+showBalance() {
+ const expense = this.sumOfExpenses();
  const total = parseInt(this.budgetTotal.textContent) - expense;
  this.balanceTotal.textContent = total;
-
- if (total < 0){
+if (total < 0) {
   this.balance.classList.remove("greenFont", "blackFont");
   this.balance.classList.add("redFont");
  } else if (total > 0) {
@@ -50,35 +50,77 @@ showBalance(){
   this.balance.classList.add("blackFont");
  }
 }
-sumofExpenses(){
+enterExpenses() {
+  const expenseValue = this.expenseInput.value;
+  const memoValue = this.memoInput.value;
+  if (memoValue === "" || expenseValue ===  "" || expenseValue < 0) {
+  this.expenseMessage.classList.add("showMessage");
+  this.expenseMessage.innerHTML =
+   `<div>Fields cannot be negative or empty.</div>`;
+const refer = this;
+setTimeout(() => {
+  refer.expenseMessage.classList.remove("showMessage");
+}, 4000);
+} else {
+  let amount = parseInt(memoValue);
+  this.expenseInput.value = "";
+  this.memoInput.value = "";
+
+  let expense ={
+    id: this.itemID,
+    title: expenseValue,
+    amount: amount,
+  }
+  this.itemID++;
+  this.listItem.push(expense);
+  this.addExpense(expense);
+//show Balance
+}
+}
+addExpense(expense){
+  const div = document.createElement('div');
+  div.classList.add('expense');
+  div.innerHTML =
+    `
+  <div class="expense-item d-flex justify-content-between align-items-baseline">
+   <h6 class="expense-title mb-0 text-uppercase list-item">title</h6>
+   <h5 class="expense-amount mb-0 list-item">amount</h5>
+   <div class="expense-icons list-item">
+    <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+     <i class="fas fa-edit"></i>
+    </a>
+    <a href="#" class="delete-icon" data-id="${expense.id}">
+     <i class="fas fa-trash"></i>
+    </a>
+   </div>`;
+
+}
+sumOfExpenses() {
   let total = 200;
   return total;
  }
- } 
-
-function eventListeners (){
+}
+function eventListeners() {
 const budgetForm = document.getElementById("budget-form");
 const expenseForm = document.getElementById("expense-form");
 const expenseList = document.getElementById("expense-list");
 
 
 //new instance of UI class
-const ui = new UI()
+const ui = new UI();
 
-budgetForm.addEventListener("submit", function(event){
+budgetForm.addEventListener("submit", function(event) {
 event.preventDefault();
 ui.enterBudget();
-})
 
-expenseForm.addEventListener("submit", function(event){
-  event.preventDefault(); 
-  ui.enterExpense();
-})
+});
+expenseForm.addEventListener("submit", function(event) {
+event.preventDefault(); 
+ui.enterExpenses();
+});
 
-expenseList.addEventListener("click", function(){
-
-})
+expenseList.addEventListener("click", function() {});
 }
-document.addEventListener ('DOMContentLoaded' , function(){
+document.addEventListener ("DOMContentLoaded", function() {
   eventListeners();
-})
+});
